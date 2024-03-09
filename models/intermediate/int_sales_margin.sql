30400d8ae52c92f0d50536f4d489cdc82f488658
MOHRAH70MM@GMAIL.COM <MOHRAH70MM@GMAIL.COM>
2024-03-09 10:28:56 +0000

    knb

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,11 +1,18 @@
-select
-    products_id,
-    date_date,
-    orders_id,
-    revenue,
-    quantity,
-    cast(purchase_price as float64) as purchase_price,
-    round(s.quantity * cast(p.purchase_price as float64), 2) as purchase_cost,
-    s.revenue - round(s.quantity * cast(p.purchase_price as float64), 2) as margin
-from {{ ref("stg_raw_sales") }} s
-left join {{ ref("stg_raw_product") }} p using (products_id)
+
+
+select *
+from (
+    select
+        products_id,
+        date_date,
+        orders_id,
+        revenue,
+        quantity,
+        cast(purchase_price as float64) as purchase_price,
+        round(s.quantity * cast(p.purchase_price as float64), 2) as purchase_cost,
+        s.revenue - round(s.quantity * cast(p.purchase_price as float64), 2) as margin,
+        round( SAFE_DIVIDE( (s.revenue - s.quantity * CAST(p.purchase_price AS float64)) , s.revenue ) , 2) as margin_percentage
+    from `river-cocoa-414101`.`dbt_mohrah`.`stg_raw_sales` s
+    left join `river-cocoa-414101`.`dbt_mohrah`.`stg_raw_product` p using (products_id)
+) as model_limit_subq
+limit 500
